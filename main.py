7fcd27e413520958e7f6b1830db7b91ea2da848f
James <jamestariga@live.com>
2021-12-05 19:12:06 -0800

    Major Update

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ classFile = 'coco.names'
 with open(classFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
+# Windows version, comment line below if running in raspberry pi
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+
+# Raspberry Pi version, remove '#' on the next line
+# configPath = '/home/pi/Desktop/ObjectDetection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+
+# Windows version, comment line below if running in raspberry pi
 weightsPath = 'frozen_inference_graph.pb'
+
+# Raspberry Pi version, remove '#' on the next line
+# weightsPath = '/home/pi/Desktop/ObjectDetection/frozen_inference_graph.pb'
 
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
 net.setInputSize(320, 320)
@@ -21,13 +30,15 @@ net.setInputSwapRB(True)
 
 while True:
     success, img = cap.read()
-    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=0.2)
 
     if len(classIds) != 0:
         for classId, confidence,box in zip(classIds.flatten(), confs.flatten(), bbox):
             cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-            cv2.putText(img, classNames[classId - 1], (box[0] + 10, box[1] + 30),
+            cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
                         cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-    cv2.imshow("Object Detection",img)
+    cv2.imshow("Object Detection", img)
     cv2.waitKey(1)
